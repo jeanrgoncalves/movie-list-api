2ec49151a4c54f172df8fd7ddb549d09f9346729
jeanrgoncalves <virgulinos_18@hotmail.com>
2021-04-20 17:18:47 -0300

    - Dockerfile - message_en para rodar no container (problema do locale no container) - url do mongo para o container acessar o host

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,9 @@
-FROM maven:3.6.3-openjdk
+FROM maven:3.6.3-openjdk-11
 
 COPY src home/movie-list-api/src
 COPY pom.xml home/movie-list-api
 
-WORKDIR home/movie-list-api
+WORKDIR /home/movie-list-api
 RUN mvn clean package
 
-WORKDIR home/movie-list-api/target
-
-EXPOSE 8443 5432
-
-ENTRYPOINT ["java", "-jar", "movielist-0.0.1-SNAPSHOT.jar"]
-
+ENTRYPOINT ["java", "-jar", "/home/movie-list-api/target/movielist-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/movielist/movielist/util/Translator.java
+++ src/main/java/com/movielist/movielist/util/Translator.java
@@ -1,5 +1,6 @@
 package com.movielist.movielist.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.text.MessageFormat;
@@ -7,12 +8,15 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 @Component
+@Slf4j
 public class Translator {
 
     private Locale locale = Locale.getDefault();
     ResourceBundle resourceMessages = ResourceBundle.getBundle("i18n.messages", locale);
 
     public String getText(String messageCode, String... parameters) {
+        log.info(locale.toString());
+        log.info(locale.getLanguage());
          String text = resourceMessages.getString(messageCode);
         return MessageFormat.format(text, parameters);
     }
