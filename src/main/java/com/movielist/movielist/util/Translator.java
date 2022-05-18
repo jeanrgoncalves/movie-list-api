92be5fe835cc5227f72ed24e9d1ba62c127b55af
jeanrgoncalves <jeanrgoncalves@hotmail.com>
2022-05-18 07:52:01 -0300

    Banco local

--- src/main/java/com/movielist/movielist/util/Translator.java
+++ src/main/java/com/movielist/movielist/util/Translator.java
@@ -4,20 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.text.MessageFormat;
-import java.util.Locale;
 import java.util.ResourceBundle;
 
 @Component
 @Slf4j
 public class Translator {
 
-    private Locale locale = Locale.getDefault();
-    ResourceBundle resourceMessages = ResourceBundle.getBundle("i18n.messages", locale);
+    ResourceBundle resourceMessages = ResourceBundle.getBundle("i18n.messages");
 
     public String getText(String messageCode, String... parameters) {
-        log.info(locale.toString());
-        log.info(locale.getLanguage());
-         String text = resourceMessages.getString(messageCode);
+        String text = resourceMessages.getString(messageCode);
         return MessageFormat.format(text, parameters);
     }
 
